450f30f8f5cc5599e704c39ec6507c40b9dd896b
Chakritha perla <perlachakritha@gmail.com>
2026-05-15 00:59:18 +0530

    backend project ready

--- server.js
+++ server.js
@@ -22,7 +22,17 @@ app.get("/", (req, res) => {
     res.send("API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+app.post("/tasks", createTask);
+
+app.get("/tasks", getTasks);
+
+app.get("/tasks/:id", getTask);
+
+app.put("/tasks/:id", updateTask);
+
+app.delete("/tasks/:id", deleteTask);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
